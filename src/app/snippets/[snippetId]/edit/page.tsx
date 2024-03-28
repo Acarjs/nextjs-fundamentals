c9ccf767118fa43a39cd420db2bc7dc7d4637125
Anil Acar <ostravba@gmail.com>
2024-03-28 10:57:45 +0100

    snippet-edit-form page add

--- src/app/snippets/[snippetId]/edit/page.tsx
+++ src/app/snippets/[snippetId]/edit/page.tsx
@@ -1,11 +1,29 @@
+import { db } from '@/db';
+import { notFound } from 'next/navigation';
+import SnippetEditForm from '@/components/snippet-edit-form';
+
 type SnippetEditPageProps = {
   params: {
     snippetId: string;
   };
 };
 
-export default function SnippetEditPage({ params }: SnippetEditPageProps) {
+export default async function SnippetEditPage({
+  params,
+}: SnippetEditPageProps) {
   const id = +params.snippetId;
 
-  return <div>Editing snippet with id {id}</div>;
+  const snippet = await db.snippet.findFirst({
+    where: { id: id },
+  });
+
+  if (!snippet) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      <SnippetEditForm snippet={snippet} />
+    </div>
+  );
 }
