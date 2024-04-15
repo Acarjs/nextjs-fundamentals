9b7bf7d0642e2d79f36bd00389f1d96e735c1671
Anil Acar <ostravba@gmail.com>
2024-04-15 11:59:17 +0200

    error handling

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -28,31 +28,43 @@ export async function createSnippet(
   formState: formStateProps,
   formData: FormData
 ) {
-  //check the user's inputs and make sure they're valid.
-  const title = formData.get('title');
-  const code = formData.get('code');
+  try {
+    //check the user's inputs and make sure they're valid.
+    const title = formData.get('title');
+    const code = formData.get('code');
 
-  if (typeof title !== 'string' || title.length < 3) {
-    return {
-      message: 'Please enter a valid title',
-    }; //we must retunr the same type as whatever that initial state type was that we passed in to our hook.
+    if (typeof title !== 'string' || title.length < 3) {
+      return {
+        message: 'Please enter a valid title',
+      }; //we must retunr the same type as whatever that initial state type was that we passed in to our hook.
+    }
+
+    if (typeof code !== 'string' || code.length < 3) {
+      return {
+        message: 'Please enter a valid code',
+      };
+    }
+
+    //create a new record in the database;
+    const snippet = await db.snippet.create({
+      data: {
+        title: title,
+        code: code,
+      },
+    });
+    console.log(snippet);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        message: err.message,
+      };
+    } else {
+      return {
+        message: 'Something went wrong...',
+      };
+    }
   }
 
-  if (typeof code !== 'string' || code.length < 3) {
-    return {
-      message: 'Please enter a valid code',
-    };
-  }
-
-  //create a new record in the database;
-  const snippet = await db.snippet.create({
-    data: {
-      title: title,
-      code: code,
-    },
-  });
-  console.log(snippet);
-
-  //redirect the usere back to the root route
+  //redirect the user back to the root route
   redirect('/');
 } //this is a server action
